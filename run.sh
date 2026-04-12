55fb45f149f2984e0c4216ff875b3b1de493b559
@birtdata.com <kbirt@birtdata.com>
2026-04-12 16:13:02 -0400

    use h200

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=gpu-interactive
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --gres=gpu:h200:1
 #SBATCH --ntasks=1
 #SBATCH --mem=16GB
-#SBATCH --time=02:00:00
+#SBATCH --time=00:30:00
 /home/$(whoami)/.local/bin/boltzgen run /home/$(whoami)/bg-a-synuclein/$CONFIG --cache=/scratch/$(whoami)/bg-cache/ --output=/scratch/$(whoami)/bg-out/ --protocol=protein-anything --num_designs=$NUM_DESIGNS --budget=2
