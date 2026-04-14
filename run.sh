a37288689ad42737b01334dae007d2451bf9c468
@birtdata.com <kbirt@birtdata.com>
2026-04-13 22:19:07 -0400

    remove cache pre-run

--- run.sh
+++ run.sh
@@ -5,4 +5,6 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=16GB
 #SBATCH --time=02:00:00
+rm -r /scratch/$(whoami)/bg-cache/
+mkdir -p /scratch/$(whoami)/bg-cache/
 /home/$(whoami)/.local/bin/boltzgen run /home/$(whoami)/bg-a-synuclein/$CONFIG --cache=/scratch/$(whoami)/bg-cache/ --output=/scratch/$(whoami)/bg-out/ --protocol=protein-anything --num_designs=$NUM_DESIGNS --budget=2
